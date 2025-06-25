96459dd5e8470dd7f3f2936163bb2a3c319b7016
BrayanPulido06 <brayan06.pulido@gmail.com>
2025-06-25 10:11:28 -0500

    botones dinamicos

---
+++ public/js/scripts.js
@@ -0,0 +1,56 @@
+//botones mostrar info
+function mostrar1() {
+    document.getElementById('tarifa1').style.display = 'block';
+}
+
+function ocultar1() {
+    document.getElementById('tarifa1').style.display = 'none';
+}
+
+function mostrar2() {
+    document.getElementById('tarifa2').style.display = 'block';
+}
+
+function ocultar2() {
+    document.getElementById('tarifa2').style.display = 'none';
+}
+
+function mostrar3() {
+    document.getElementById('tarifa3').style.display = 'block';
+}
+
+function ocultar3() {
+    document.getElementById('tarifa3').style.display = 'none';
+}
+
+function mostrar4() {
+    document.getElementById('tarifa4').style.display = 'block';
+}
+
+function ocultar4() {
+    document.getElementById('tarifa4').style.display = 'none';
+}
+
+function mostrar5() {
+    document.getElementById('tarifa5').style.display = 'block';
+}
+
+function ocultar5() {
+    document.getElementById('tarifa5').style.display = 'none';
+}
+
+function mostrar6() {
+    document.getElementById('tarifa6').style.display = 'block';
+}
+
+function ocultar6() {
+    document.getElementById('tarifa6').style.display = 'none';
+}
+
+function mostrar7() {
+    document.getElementById('tarifa7').style.display = 'block';
+}
+
+function ocultar7() {
+    document.getElementById('tarifa7').style.display = 'none';
+}
