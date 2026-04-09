ddb675dea9584bcabbc5407ff20c0480b2552cf3
BrayanPulido06 <brayan06.pulido@gmail.com>
2026-04-09 09:25:19 -0500

    conexion

--- models/conexionGlobal.php
+++ models/conexionGlobal.php
@@ -2,8 +2,8 @@
 function conexionDB(){
     try {
         // IMPORTANTE: Si tu XAMPP dice Port: 3307, cambia el 3306 de abajo por 3307
-        $dsn = "mysql:host=localhost;port=3306;dbname=u478643953_ecobikemess;charset=utf8";
-        $db = new PDO($dsn, 'u478643953_ecobikemess', '/Brayan3675602');
+        $dsn = "mysql:host=localhost;dbname=u478643953_ecobikemess;charset=utf8";
+        $db = new PDO($dsn, 'u478643953_ecobikemess', '/EcoBikeMess0310');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
     } catch (PDOException $PDOe) {
